0c423afeb099a505bade7e81c397f260835f2c1a
Andrew McPherson <andrew.mcpherson@gmail.com>
2020-04-17 14:47:49 -0400

    latest

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ For a detailed guide see the related single cell pipeline
 
 [Changelog](CHANGELOG.md)
 
+Run on an example:
+
+```
+microscope_image_converter convert --input_yaml example/inputs.yaml --out_dir example/out/ --loglevel DEBUG --tmpdir tmp --pipelinedir pipeline --submit local
+```
--- microscope_image_converter/tasks.py
+++ microscope_image_converter/tasks.py
@@ -1,4 +1,20 @@
+import os
+import skimage
+import skimage.io
+import numpy as np
+from matplotlib import pyplot as plt
 
 
-def convert():
-    pass
+def convert(red_path, cyan_path, output_path):
+    red = skimage.io.imread(red_path)
+    red_adj = skimage.exposure.rescale_intensity(red, out_range=(0,255)).astype('uint8')
+
+    cyan = skimage.io.imread(cyan_path)
+    cyan_adj = skimage.exposure.rescale_intensity(cyan, out_range=(0,255)).astype('uint8')
+
+    zero_array = np.zeros(cyan_adj.shape).astype('uint8')
+
+    reshaped = np.dstack((red_adj, cyan_adj, zero_array))
+
+    skimage.io.imsave(output_path, reshaped, format='png')
+
